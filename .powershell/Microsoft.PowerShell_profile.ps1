0a2410072cf7dda69bb7faaaf4ae012dce192630
coffeesnwob <lindelwekhumalo@outlook.com>
2025-05-03 02:32:20 +0200

    Powershell .files

---
+++ .powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,37 @@
+<# -- Aliases -- #>
+
+# Git
+function Get-GitStatus { 
+    & git status $args 
+}
+New-Alias -Name gs -Value Get-GitStatus
+
+function Set-GitCommit {
+    & git commit $args
+}
+New-Alias -Name c -Value Set-GitCommit
+
+function Get-GitAdd {
+    & git add $args
+}
+New-Alias -Name a -Value Get-GitAdd
+
+function Get-GitPush {
+    & git push $args
+}
+New-Alias -Name p -Value Get-GitPush
+
+<# -- Shell Apppearance -- #>
+
+# starship config
+<# $ENV:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
+Invoke-Expression (&starship init powershell) #>
+
+# oh-my posh
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\negligible.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\pure.omp.json" | Invoke-Expression
+
+
+<# -- Module -- #>
+Import-Module -Name Terminal-Icons
+
